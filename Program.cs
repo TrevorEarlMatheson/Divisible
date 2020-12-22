e85b9fe1ea9950dcafd231772134e9133d4878b4
Trevor Earl Matheson <trevor.earl.matheson@gmail.com>
2020-12-22 12:34:13 -0700

    Created function that counts dividens for 3 from 1 to 100.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Divisible
+{
+    class Program
+    {
+
+        static int Divisible(int dividend, int divisibleCount)
+        {
+            int end = 100;
+            int divisor = 3;
+            if(dividend % divisor == 0) // check if the dividend is divisible. If so add one to the count.
+            {
+                divisibleCount += 1;
+            }
+            if(dividend <= end) //If the dividend is not at the limit we recursively call the function to check the next number up. Otherwise we return our count.
+            {
+                return(Divisible(++dividend, divisibleCount));
+            }
+            else
+            {
+                return(divisibleCount);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divisible(1,0));
+        }
+    }
+}
